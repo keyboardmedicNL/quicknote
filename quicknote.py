40826d4f27f5a04f8274f14eee31eec4534ba8b4
KeyboardMedic <keyboardmedic@proton.me>
2023-09-19 19:12:55 +0200

    added comments t o scripts

--- quicknote.py
+++ quicknote.py
@@ -2,11 +2,11 @@
 import sys
 
 # define these variables before use
-notefile = "quicknote.md"
+notefile = "quicknote.md" # full path of note file
 
 # main script
-a = str(sys.argv[1])
-with open(notefile, "a") as myfile:
-    myfile.write("\n")
-    myfile.write(a + "\n")
-print("noting down: " + a)
+a = str(sys.argv[1]) # pulls first argument
+with open(notefile, "a") as myfile: # opens file in append mode
+    myfile.write("\n") # writes blank line
+    myfile.write(a + "\n") # writes argument to file
+print("noting down: " + a) # log message
